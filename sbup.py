cddc0186d3bbc338a0d6f03811dd1bf94b11cf3a
srazojr <srazojr@ufl.edu>
2015-03-14 16:56:31 -0400

    add help

--- sbup.py
+++ sbup.py
@@ -25,6 +25,27 @@ sidebar_contents = settings['description']
 
 
 
+
+a="""sbup.config()     parameters: ("user","pass", "subreddit","date")
+                  subreddit and date are optional and default to 
+			      metalgearsolid and september 1st of this year. 
+				  The subreddit format does not include /r/ so 
+				  "metalgearsolid" is correct not "/r/metalgearsolid"
+				  The date format is: "MM DD YYYY HH:MMXM" where the 
+				  ending "XM" is AM or PM.
+				  
+sbup.init()       No parameters, will create the first line at the
+                  end of the sidebar. The exact action of this function
+				  is to download the sidebar, add two spaces, a newline
+				  and a GT symbol, and the "XXX days remaining. "
+				 
+sbup.update()     No parameters, will manually update the number once. 
+sbup.run()        No parameters. Will call update at midnight every day 
+                  until the day count is less than 0. 
+				  
+				 """
+
+
 #Update the settings
 
 r.update_settings(r.get_subreddit(sub), description=sidebar_contents)
